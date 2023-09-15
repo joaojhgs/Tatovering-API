ea517b0f0ef7f601cf48e19e0b4b62c618063a8f
joaojhgs <joaosperandio@alunos.utfpr.edu.br>
2023-09-15 17:10:54 -0300

    Initial jwtChecking middleware

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"net/http"
 	"context"
+	"errors"
+	"strings"
 	"github.com/gin-gonic/gin"
 	supabase "github.com/nedpals/supabase-go"
 )
@@ -13,72 +15,99 @@ func main() {
 	supabaseURL := "https://pfzlboeaonsookzcnniv.supabase.co"
 	supabaseKey := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBmemxib2Vhb25zb29remNubml2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTIwNjI3NTYsImV4cCI6MjAwNzYzODc1Nn0.KuEEX9EBIQmLTA02iPtqqNIewDmXITDxnIfD4qEqTN8"
 
+	extractBearerToken := func (header string) (string, error) {
+		if header == "" {
+			return "", errors.New("bad header value given")
+		}
+	
+		jwtToken := strings.Split(header, " ")
+		if len(jwtToken) != 2 {
+			return "", errors.New("incorrectly formatted authorization header")
+		}
+	
+		return jwtToken[1], nil
+	}
+	
+	
+	jwtTokenCheck := func (c *gin.Context) {
+		jwtToken, err := extractBearerToken(c.GetHeader("Authorization"))
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		client := supabase.CreateClient(supabaseURL, supabaseKey)
+		client.DB.AddHeader("Authorization", "Bearer "+jwtToken);
+		c.Next()
+	}
+
 	// Create a Gin router
 	router := gin.Default()
 
-// Route for user sign-up
-router.POST("/signup", func(c *gin.Context) {
-	var requestBody struct {
-		Email    string `json:"email"`
-		Password string `json:"password"`
-	}
+	private := router.Group("/private", jwtTokenCheck);
+	
+	// Route for user sign-up
+	router.POST("/signup", func(c *gin.Context) {
+		var requestBody struct {
+			Email    string `json:"email"`
+			Password string `json:"password"`
+		}
 
-	if err := c.ShouldBindJSON(&requestBody); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
-	client := supabase.CreateClient(supabaseURL, supabaseKey)
+		if err := c.ShouldBindJSON(&requestBody); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		client := supabase.CreateClient(supabaseURL, supabaseKey)
 
-	// Create user credentials
-	credentials := supabase.UserCredentials{
-		Email:    requestBody.Email,
-		Password: requestBody.Password,
-	}
-	ctx := context.Background()
-	// Sign up the user with Supabase
-	user, err := client.Auth.SignUp(ctx, credentials)
+		// Create user credentials
+		credentials := supabase.UserCredentials{
+			Email:    requestBody.Email,
+			Password: requestBody.Password,
+		}
+		ctx := context.Background()
+		// Sign up the user with Supabase
+		user, err := client.Auth.SignUp(ctx, credentials)
 
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-		return
-	}
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
 
-	c.JSON(http.StatusCreated, gin.H{"user": user})
-})
+		c.JSON(http.StatusCreated, gin.H{"user": user})
+	})
 
-// Route for user sign-in
-router.POST("/signin", func(c *gin.Context) {
-	var requestBody struct {
-		Email    string `json:"email"`
-		Password string `json:"password"`
-	}
+	// Route for user sign-in
+	router.POST("/signin", func(c *gin.Context) {
+		var requestBody struct {
+			Email    string `json:"email"`
+			Password string `json:"password"`
+		}
 
-	if err := c.ShouldBindJSON(&requestBody); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
-	client := supabase.CreateClient(supabaseURL, supabaseKey)
+		if err := c.ShouldBindJSON(&requestBody); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		client := supabase.CreateClient(supabaseURL, supabaseKey)
 
-	// Create user credentials
-	credentials := supabase.UserCredentials{
-		Email:    requestBody.Email,
-		Password: requestBody.Password,
-	}
+		// Create user credentials
+		credentials := supabase.UserCredentials{
+			Email:    requestBody.Email,
+			Password: requestBody.Password,
+		}
 
-	ctx := context.Background()
-	// Sign up the user with Supabase
-	user, err := client.Auth.SignIn(ctx, credentials)
+		ctx := context.Background()
+		// Sign up the user with Supabase
+		user, err := client.Auth.SignIn(ctx, credentials)
 
-	if err != nil {
-		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid credentials"})
-		return
-	}
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid credentials"})
+			return
+		}
 
-	c.JSON(http.StatusOK, gin.H{"user": user})
-})
+		c.JSON(http.StatusOK, gin.H{"user": user})
+	})
 
 	// Define CRUD routes for "usuarios"
-	router.POST("/usuarios", func(c *gin.Context) {
+	private.POST("/usuarios", func(c *gin.Context) {
 		// Create a new usuario
 		row := Usuario{
 			Nome: "Gabriel Medrado",
@@ -94,7 +123,7 @@ router.POST("/signin", func(c *gin.Context) {
 
 			c.JSON(http.StatusCreated, results)
 	})
-	
+
 	// Start the Gin server
 	port := 8080 // Change to the desired port
 	router.Run(fmt.Sprintf(":%d", port))
