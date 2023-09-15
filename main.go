c3e914df85e0c9f1b8b1c5ab2da6e09023040320
joaojhgs <joaosperandio@alunos.utfpr.edu.br>
2023-09-15 16:43:02 -0300

    #7 - Signin e Signup routes

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
-
+	"context"
 	"github.com/gin-gonic/gin"
 	supabase "github.com/nedpals/supabase-go"
 )
@@ -16,78 +16,85 @@ func main() {
 	// Create a Gin router
 	router := gin.Default()
 
+// Route for user sign-up
+router.POST("/signup", func(c *gin.Context) {
+	var requestBody struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+
+	if err := c.ShouldBindJSON(&requestBody); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	client := supabase.CreateClient(supabaseURL, supabaseKey)
+
+	// Create user credentials
+	credentials := supabase.UserCredentials{
+		Email:    requestBody.Email,
+		Password: requestBody.Password,
+	}
+	ctx := context.Background()
+	// Sign up the user with Supabase
+	user, err := client.Auth.SignUp(ctx, credentials)
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{"user": user})
+})
+
+// Route for user sign-in
+router.POST("/signin", func(c *gin.Context) {
+	var requestBody struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+
+	if err := c.ShouldBindJSON(&requestBody); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	client := supabase.CreateClient(supabaseURL, supabaseKey)
+
+	// Create user credentials
+	credentials := supabase.UserCredentials{
+		Email:    requestBody.Email,
+		Password: requestBody.Password,
+	}
+
+	ctx := context.Background()
+	// Sign up the user with Supabase
+	user, err := client.Auth.SignIn(ctx, credentials)
+
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid credentials"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"user": user})
+})
+
 	// Define CRUD routes for "usuarios"
 	router.POST("/usuarios", func(c *gin.Context) {
 		// Create a new usuario
-    row := Usuario{
-      Nome: "Gabriel Medrado",
-    }
-    var results []Usuario
-    client := supabase.CreateClient(supabaseURL, supabaseKey)
-    err := client.DB.From("usuarios").Insert(row).Execute(&results)
-    
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-			return
+		row := Usuario{
+			Nome: "Gabriel Medrado",
 		}
+		var results []Usuario
+		client := supabase.CreateClient(supabaseURL, supabaseKey)
+		err := client.DB.From("usuarios").Insert(row).Execute(&results)
 
-		c.JSON(http.StatusCreated, results)
+			if err != nil {
+				c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+				return
+			}
+
+			c.JSON(http.StatusCreated, results)
 	})
-
-	// router.GET("/usuarios/:id", func(c *gin.Context) {
-	// 	// Get a usuario by ID
-	// 	tatuadorID := c.Param("id")
-	// 	resp, err := client.
-	// 		Table("usuarios").
-	// 		Select("*").
-	// 		Equals("id", tatuadorID).
-	// 		Single()
-	// 	if err != nil {
-	// 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-	// 		return
-	// 	}
-
-	// 	c.JSON(http.StatusOK, resp)
-	// })
-
-	// router.PUT("/usuarios/:id", func(c *gin.Context) {
-	// 	// Update a usuario by ID
-	// 	tatuadorID := c.Param("id")
-	// 	var usuario Usuario // Define the Usuario struct as needed
-	// 	if err := c.BindJSON(&usuario); err != nil {
-	// 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-	// 		return
-	// 	}
-
-	// 	resp, err := client.
-	// 		Table("usuarios").
-	// 		Update(tatuadorToMap(usuario)).
-	// 		Equals("id", tatuadorID).
-	// 		Single()
-	// 	if err != nil {
-	// 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-	// 		return
-	// 	}
-
-	// 	c.JSON(http.StatusOK, resp)
-	// })
-
-	// router.DELETE("/usuarios/:id", func(c *gin.Context) {
-	// 	// Delete a usuario by ID
-	// 	tatuadorID := c.Param("id")
-	// 	resp, err := client.
-	// 		Table("usuarios").
-	// 		Delete().
-	// 		Equals("id", tatuadorID).
-	// 		Single()
-	// 	if err != nil {
-	// 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-	// 		return
-	// 	}
-
-	// 	c.JSON(http.StatusOK, resp)
-	// })
-
+	
 	// Start the Gin server
 	port := 8080 // Change to the desired port
 	router.Run(fmt.Sprintf(":%d", port))
