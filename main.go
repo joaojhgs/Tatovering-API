ef69ee5a469b37fc32f8e5c860cfe40f8039fabb
hebertCardoso <hebert.carmo@appmoove.com.br>
2023-09-18 20:14:26 -0300

    # 4 - sem do

--- main.go
+++ main.go
@@ -7,13 +7,20 @@ import (
 	"errors"
 	"strings"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"os"
+	"github.com/jinzhu/copier"
 	supabase "github.com/nedpals/supabase-go"
 )
 
 func main() {
+	if err := godotenv.Load(); err != nil {
+        fmt.Printf("Erro ao carregar arquivo .env: %v\n", err)
+        os.Exit(1)
+    }
 	// Initialize your Supabase client
-	supabaseURL := "https://pfzlboeaonsookzcnniv.supabase.co"
-	supabaseKey := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBmemxib2Vhb25zb29remNubml2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTIwNjI3NTYsImV4cCI6MjAwNzYzODc1Nn0.KuEEX9EBIQmLTA02iPtqqNIewDmXITDxnIfD4qEqTN8"
+	supabaseURL := os.Getenv("SUPABASE_URL")
+	supabaseKey := os.Getenv("SUPABASE_KEY")
 
 	extractBearerToken := func (header string) (string, error) {
 		if header == "" {
@@ -49,35 +56,6 @@ func main() {
 	
 	//Initialize a single supabase client instead of one for each query received
 	client := supabase.CreateClient(supabaseURL, supabaseKey)
-	// Route for user sign-up
-	router.POST("/signup", func(c *gin.Context) {
-		// Defines the input data and validation
-		var requestBody struct {
-			Email    string `json:"email"`
-			Password string `json:"password"`
-		}
-		// Bind the request to the defined model and throw error if some validation fails.
-		if err := c.ShouldBindJSON(&requestBody); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-			return
-		}
-
-		// Create user credentials
-		credentials := supabase.UserCredentials{
-			Email:    requestBody.Email,
-			Password: requestBody.Password,
-		}
-		ctx := context.Background()
-		// Sign up the user with Supabase
-		user, err := client.Auth.SignUp(ctx, credentials)
-
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-			return
-		}
-
-		c.JSON(http.StatusCreated, gin.H{"user": user})
-	})
 
 	// Route for user sign-in
 	router.POST("/signin", func(c *gin.Context) {
@@ -101,6 +79,7 @@ func main() {
 		// Sign up the user with Supabase
 		user, err := client.Auth.SignIn(ctx, credentials)
 
+		fmt.Println(user)
 		if err != nil {
 			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid credentials"})
 			return
@@ -112,12 +91,18 @@ func main() {
 	// Define CRUD routes for "usuarios"
 	private.POST("/usuarios", func(c *gin.Context) {
 		// Create a new usuario
-		row := Usuario{
-			Nome: "Gabriel Medrado",
+		var usuario Usuario
+
+		// BindJSON tentará analisar o corpo da solicitação JSON na variável 'usuario'
+		if err := c.BindJSON(&usuario); err != nil {
+			// Em caso de erro, responder com status de erro e mensagem
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
 		}
+
 		var results []Usuario
 		client := supabase.CreateClient(supabaseURL, supabaseKey)
-		err := client.DB.From("usuarios").Insert(row).Execute(&results)
+		err := client.DB.From("usuarios").Insert(usuario).Execute(&results)
 
 			if err != nil {
 				c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
@@ -127,6 +112,62 @@ func main() {
 			c.JSON(http.StatusCreated, results)
 	})
 
+	router.POST("/tatuadores", func(c *gin.Context) {
+		client := supabase.CreateClient(supabaseURL, supabaseKey)
+		// Crie uma variável para armazenar os dados do usuário a partir do corpo da solicitação
+
+		var data map[string]map[string]interface{}
+		
+		// BindJSON tentará analisar o corpo da solicitação JSON na variável 'usuario'
+		if err := c.BindJSON(&data); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Erro ao decodificar JSON"})
+			return
+		}
+
+		usuarioData := data["usuario"]
+		tatuadorData := data["tatuador"] 
+		
+		fmt.Println("AQUI", usuarioData)
+		fmt.Println("AQUI", tatuadorData)
+		
+		var usuario Usuario
+		var tatuador Tatuador
+
+		if usuarioData != nil {
+			// Use o pacote github.com/jinzhu/copier para copiar os valores do mapa para a struct
+			if err := copier.Copy(&usuario, usuarioData); err != nil {
+				c.JSON(http.StatusInternalServerError, gin.H{"error": "Erro ao copiar dados para a struct de usuario"})
+				return
+			}
+		}
+
+		if tatuadorData != nil {
+			// Use o pacote github.com/jinzhu/copier para copiar os valores do mapa para a struct
+			if err := copier.Copy(&tatuador, tatuadorData); err != nil {
+				c.JSON(http.StatusInternalServerError, gin.H{"error": "Erro ao copiar dados para a struct de tatuador"})
+				return
+			}
+		}
+
+		var resul []Usuario
+
+		err := client.DB.From("usuarios").Insert(usuario).Execute(&resul)
+			if err != nil {
+				c.JSON(http.StatusInternalServerError, gin.H{"caca": err.Error()})
+				return
+			} else {
+				var results []Tatuador
+
+				err := client.DB.From("tatuadores").Insert(tatuador).Execute(&results)
+				if err != nil {
+					c.JSON(http.StatusInternalServerError, gin.H{"ruim": err.Error()})
+					return
+				}
+
+				c.JSON(http.StatusCreated, results)
+			}
+	})
+
 	// Start the Gin server
 	port := 8080 // Change to the desired port
 	router.Run(fmt.Sprintf(":%d", port))
@@ -134,12 +175,25 @@ func main() {
 
 // Define the Usuario struct to match your database structure
 type Usuario struct {
-  Nome    string `json:"nome"`
+	Nome string `json:"nome"`
+	Email string `json:"email"`
+	TelefoneCelular string `json:"telefone_celular"`
+	Cpf string `json:"cpf"`
+	Rg string `json:"rg"`
+	Status string `json:"status"`
+	Endereco string `json:"endereco"`
 }
 
-// Helper function to convert Usuario struct to map for Supabase
-func tatuadorToMap(usuario Usuario) map[string]interface{} {
-	return map[string]interface{}{
-		"nome":      usuario.Nome,
-	}
+type Tatuador struct {
+	UsuarioUuid string `json:"usuario_uuid"`
+	EstudioId int `json:"estudio_id"`
+	Experiencia int `json:"experiencia"`
+	EstiloTatuagem string `json:"estilo_tatuagem"`
+	Status string `json:"status"`
+	Tipo string `json:"tipo"`
+	RedesSociais   *struct {
+		Instagram string `json:"instagram"`
+		X         string `json:"x"`
+		Facebook  string `json:"facebook"`
+	} `json:"redes_sociais"`
 }
