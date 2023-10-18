437375ac9937788a0d3c751ca8c4232ddd1a3efb
Matheus Fontes <matt.fontes@hotmail.com>
2023-10-18 20:37:34 -0300

    Feat: Fix insano no problema enorme de CORS do projeto. Esse problema era muito grande

--- src/routes/estudio.go
+++ src/routes/estudio.go
@@ -1,31 +1,32 @@
 package routes
 
 import (
-    "github.com/gin-gonic/gin"
-    "tatovering/src/controllers"
-    supabase "github.com/nedpals/supabase-go"
-    "tatovering/src/middlewares"
+	"tatovering/src/controllers"
+	"tatovering/src/middlewares"
+
+	"github.com/gin-gonic/gin"
+	supabase "github.com/nedpals/supabase-go"
 )
 
 func SetupEstudiosRoutes(router *gin.Engine, client *supabase.Client) {
-    estudioGroup := router.Group("/estudios")
-    {
-        estudioGroup.GET("/", controllers.ListagemEstudio(client))
-        estudioGroup.GET("/:id", controllers.GetByIdEstudio(client))
-        estudioGroup.POST(
-            "/",
-            middlewares.JwtTokenCheck(client),
-            controllers.CadastrarEstudio(client), 
-        )
-        estudioGroup.PATCH(
-            "/:id",
-            middlewares.JwtTokenCheck(client),
-            controllers.EditarEstudio(client),
-        )
-        estudioGroup.DELETE(
-            "/:id",
-            middlewares.JwtTokenCheck(client),
-            controllers.DeletarEstudio(client),
-        )
-    }
+	estudioGroup := router.Group("estudios")
+	{
+		estudioGroup.GET("", controllers.ListagemEstudio(client))
+		estudioGroup.GET(":id", controllers.GetByIdEstudio(client))
+		estudioGroup.POST(
+			"/",
+			middlewares.JwtTokenCheck(client),
+			controllers.CadastrarEstudio(client),
+		)
+		estudioGroup.PATCH(
+			"/:id",
+			middlewares.JwtTokenCheck(client),
+			controllers.EditarEstudio(client),
+		)
+		estudioGroup.DELETE(
+			"/:id",
+			middlewares.JwtTokenCheck(client),
+			controllers.DeletarEstudio(client),
+		)
+	}
 }
--- src/routes/tatuador.go
+++ src/routes/tatuador.go
@@ -1,36 +1,37 @@
 package routes
 
 import (
-    "github.com/gin-gonic/gin"
-    "tatovering/src/controllers"
-    supabase "github.com/nedpals/supabase-go"
-    "tatovering/src/middlewares"
+	"tatovering/src/controllers"
+	"tatovering/src/middlewares"
+
+	"github.com/gin-gonic/gin"
+	supabase "github.com/nedpals/supabase-go"
 )
 
 func SetupTatuadoresRoutes(router *gin.Engine, client *supabase.Client) {
-    tatuadoreGroup := router.Group("/tatuadores")
-    {
-        tatuadoreGroup.GET(
-            "/",
-            controllers.ListagemTatuador(client),
-        )
-        tatuadoreGroup.GET(
-            "/:id",
-            controllers.GetByIdTatuador(client),
-        )
-        tatuadoreGroup.GET(
-            "/estudios/:estudio_id",
-            controllers.TatuadoresEstudio(client),
-        )
-        tatuadoreGroup.POST(
-            "/",
-            middlewares.JwtTokenCheck(client),
-            controllers.CadastroTatuador(client),
-        )
-        tatuadoreGroup.PATCH(
-            "/:id",
-            middlewares.JwtTokenCheck(client),
-            controllers.EdicaoTatuador(client),
-        )
-    }
+	tatuadoreGroup := router.Group("tatuadores")
+	{
+		tatuadoreGroup.GET(
+			"",
+			controllers.ListagemTatuador(client),
+		)
+		tatuadoreGroup.GET(
+			":id",
+			controllers.GetByIdTatuador(client),
+		)
+		tatuadoreGroup.GET(
+			"estudios/:estudio_id",
+			controllers.TatuadoresEstudio(client),
+		)
+		tatuadoreGroup.POST(
+			"/",
+			middlewares.JwtTokenCheck(client),
+			controllers.CadastroTatuador(client),
+		)
+		tatuadoreGroup.PATCH(
+			"/:id",
+			middlewares.JwtTokenCheck(client),
+			controllers.EdicaoTatuador(client),
+		)
+	}
 }
