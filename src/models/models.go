73eeddbbb0ef0c8c04bc1c77c6d4abebca9a350d
Matheus Fontes <matt.fontes@hotmail.com>
2023-11-08 19:44:06 -0300

    Feat: Endereco no retorno de estudios

--- src/models/models.go
+++ src/models/models.go
@@ -1,29 +1,30 @@
 package models
 
 type Estudio struct {
-	Nome                 string `json:"nome"`
-	Email                string `json:"email"`
-	Taxa_agendamento     float64 `json:"taxa_agendamento"`
-	Localizacao 		 string `json:"localizacao"`
-	Telefone             string `json:"telefone"`
-	Descricao             string `json:"descricao"`
+	Nome                   string  `json:"nome"`
+	Email                  string  `json:"email"`
+	Taxa_agendamento       float64 `json:"taxa_agendamento"`
+	Localizacao            string  `json:"localizacao"`
+	Telefone               string  `json:"telefone"`
+	Descricao              string  `json:"descricao"`
+	Endereco               string  `json:"endereco"`
 	HorarioDeFuncionamento *struct {
 		Segunda []string `json:"segunda"`
 		Terca   []string `json:"terca"`
 		Quarta  []string `json:"quarta"`
 		Quinta  []string `json:"quinta"`
 		Sexta   []string `json:"sexta"`
-		Sabado   []string `json:"sabado"`
-		Domingo   []string `json:"domingo"`
+		Sabado  []string `json:"sabado"`
+		Domingo []string `json:"domingo"`
 	} `json:"horario_funcionamento"`
-	DiasFuncionamento    *struct {
+	DiasFuncionamento *struct {
 		Segunda bool `json:"segunda"`
 		Terca   bool `json:"terca"`
 		Quarta  bool `json:"quarta"`
 		Quinta  bool `json:"quinta"`
 		Sexta   bool `json:"sexta"`
-		Sabado   bool `json:"sabado"`
-		Domingo   bool `json:"domingo"`
+		Sabado  bool `json:"sabado"`
+		Domingo bool `json:"domingo"`
 	} `json:"dias_funcionamento"`
 }
 
@@ -37,24 +38,24 @@ type Usuario struct {
 }
 
 type Tatuador struct {
-	Nome           string    `json:"nome"`
-	Experiencia    int       `json:"experiencia"`
-	EstiloTatuagem []string    `json:"estilo_tatuagem"`
-	Status         string    `json:"status"`
-	Tipo           string    `json:"tipo"`
-	ImgemPerfil    string    `json:"imagem_perfil"`
+	Nome           string   `json:"nome"`
+	Experiencia    int      `json:"experiencia"`
+	EstiloTatuagem []string `json:"estilo_tatuagem"`
+	Status         string   `json:"status"`
+	Tipo           string   `json:"tipo"`
+	ImgemPerfil    string   `json:"imagem_perfil"`
 	RedesSociais   *struct {
-		Instagram  string    `json:"instagram"`
-		X          string    `json:"x"`
-		Facebook   string    `json:"facebook"`
+		Instagram string `json:"instagram"`
+		X         string `json:"x"`
+		Facebook  string `json:"facebook"`
 	} `json:"redes_sociais"`
 }
 
 type Tatuagem struct {
-	Imagem          string   `json:"imagem"`
-	Preco           float64  `json:"preco"`
-	Tamanho         int      `json:"tamanho"`
-	Cor             string   `json:"cor"`
-	Estilo          string   `json:"estilo"`
-	TatuadorId      string   `json:"tatuador_id"`
+	Imagem     string  `json:"imagem"`
+	Preco      float64 `json:"preco"`
+	Tamanho    int     `json:"tamanho"`
+	Cor        string  `json:"cor"`
+	Estilo     string  `json:"estilo"`
+	TatuadorId string  `json:"tatuador_id"`
 }
